0c3e52dbe31d568d2a8ff368818a2bb3a5f340d1
hannaell <hannaell91@gmail.com>
2018-11-12 11:41:10 +0100

    Made changes in the navbar
    Added lines when hovering links in the navbar and removed the backgrund color on the navbar

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 declare(strict_types=1);
 
 require __DIR__.'/header.php';
-require __DIR__.'/footer.php';
+
 ?>
+<main>
+  <img class="img1" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/screen-shot-2018-03-19-at-9-11-26-am-1521465114.png" alt="">
+</main>
 
   <script type="text/javascript" src="javascript.js"></script>
 </body>
 
 </html>
+
+ <?php
+require __DIR__.'/footer.php';
+  ?>
