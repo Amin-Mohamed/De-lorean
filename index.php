dbd5492260d38185f7a9ef686ce93db883eb659f
hannaell <hannaell91@gmail.com>
2018-11-21 11:53:19 +0100

    Added some new divs and images

--- index.php
+++ index.php
@@ -7,22 +7,38 @@ require __DIR__.'/header.php';
 <main>
 
   <div class="container">
-    <img class="img" src="/images/Preview_studio.jpg" alt="">
-    <div class="rotate">
+    <img class="img" src="/images/studioNew.png" alt="">
+    <!-- <div class="rotate">
       <h1 class="fs">FS 1.0</h1>
-    </div>
+    </div> -->
     <div class="P">
-      <h1>YOU ARE SPECIAL</h1>
+      <h1>YOU ARE SPECIAL.</h1>
       <p> FASTSECURITY 1.0</p>
     </div>
     <div class="centered">
-      <div class="button1"><a href="#">Registrera dig</a></div>
-      <div class="button2"><a href="#">Newsletter</a></div>
+      <p class="join">Join the future</p>
+      <p class="limited">And get a free limited folder</p>
+      <div class="email">
+        <div class="enter">
+          <div class="emailWhite">Your e-mail</div>
+          <div class="emailRed">Please ener your e-mail</div>
+        </div>
+        <div class="button1"><a href="#">Sign up</a></div>
+      </div>
     </div>
   </div>
+
   <div class="container2">
     <img class="img2" src="/images/2d_overlay.jpg" alt="">
   </div>
+  <div class="container3">
+    <img class="img3" src="/images/glasses.png" alt="">
+    <img class="img4" src="/images/glasses.png" alt="">
+  </div>
+  <div class="container4">
+    <img class="img5" src="/images/2d_overlay.jpg" alt="">
+  </div>
+
 </main>
 
   <script type="text/javascript" src="javascript.js"></script>
