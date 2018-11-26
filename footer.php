8f1f1e39c40088abf98e9cc15bf7ebf780a5d64c
hannaell <hannaell91@gmail.com>
2018-11-26 21:52:40 +0100

    Changed the footer

--- footer.php
+++ footer.php
@@ -1,12 +1,6 @@
-<footer class="footer-distributed">
-			<p class="footerLinks">
-				<a href="#">Saab AB © 2018</a>
-				·
-				<a href="#">Privacy & Legal</a>
-				·
-				<a href="#">Contact</a>
-				·
-				<a href="#">Get Newsletter</a>
-			</p>
-
-  </footer>
+<footer class="footer">
+                <a href="#">Saab AB © 2018</a>
+                <a href="#">Privacy & Legal</a>
+                <a href="#">Contact</a>
+                <a href="#">Get Newsletter</a>
+</footer>
