1e69dfdb1268a1d35701f29b8e2b006170d400cd
hannaell <hannaell91@gmail.com>
2018-11-29 15:31:54 +0100

    width fixed

--- script.js
+++ script.js
@@ -1,42 +1,5 @@
-//
-// // Download katalog
-// const download = document.querySelector('.download');
-//
-// console.log(download);
-// download.addEventListener('click', (event) => {
-//   console.log(download);
-//   const confirm = window.confirm('Vill du ladda ner?');
-//   if (confirm) {
-//     window.alert('Downloading');
-//   }
-//   else {
-//     window.alert('hejdå');
-//   }
-// });
-
-
-// (function() {
-//     let section = document.querySelectorAll(".section");
-//     let sections = {};
-//     let i = 0;
-//     Array.prototype.forEach.call(section, function(e) {
-//         sections[e.id] = e.offsetTop;
-//     });
-//     const active = document.querySelector('.active')
-//     window.onscroll = function() {
-//         let scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
-//         for (i in sections) {
-//             if (sections[i] <= scrollPosition) {
-//                 active.setAttribute('class', ' ');
-//                 document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
-//             }
-//         }
-//     };
-// })();
-
-
 (function() {
-  if (screen.width > 600) {
+  if (screen.width > 700) {
 
     let section = document.querySelectorAll(".section");
     let sections = {};
