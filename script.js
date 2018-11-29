55eb6be0f1abdec0afebacb25d0f2f6365bd653c
hannaell <hannaell91@gmail.com>
2018-11-29 20:02:22 +0100

    Uppdated pdf

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ for(let i = 0; i < signUp.length; i++){
 
     if(ValidateEmail(emailRight[i].value)){
       signUpError[i].style.display = 'none'
-      window.open('pdf/FastSecurity1.0.pdf', '_blank')
+      window.open('pdf/Grupp3_FS_PDF.pdf', '_blank')
     }else{
       signUpError[i].style.display = 'initial'
     }
@@ -67,7 +67,7 @@ for(let i = 0; i < signUpMobile.length; i++){
 
     if(ValidateEmail(emailRightMobile[i].value)){
       signUpErrorMobile[i].style.display = 'none'
-      window.open('pdf/FastSecurity1.0.pdf', '_blank')
+      window.open('pdf/Grupp3_FS_PDF.pdf', '_blank')
     }else{
       signUpErrorMobile[i].style.display = 'initial'
     }
